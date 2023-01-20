af23e04000830ac66d145685391cb350ece46225
shinichiro yoshida <s.erk.ikt@gmail.com>
2023-01-21 01:51:12 +0900

    Second Commit

---
+++ src/main/java/com/example/mysauna/MysaunaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysauna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysaunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysaunaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysauna/MysaunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysauna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysaunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
